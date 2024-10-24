cf530fc6f32222433e5cda4e1620088261732b9e
jethrocadang <116892945+jethrocadang@users.noreply.github.com>
2024-10-24 15:54:48 +0800

    feature(data-table): added advance faceted filter

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,14 +1,23 @@
 export interface Option {
-    label: string
-    value: string
-    icon?: React.ComponentType<{ className?: string }>
-    count?: number
-  }
+  label: string;
+  value: string;
+  icon?: React.ComponentType<{ className?: string }>;
+  count?: number;
+}
 
+export interface DataTableFilterField<TData> {
+  label: string;
+  value: keyof TData;
+  placeholder?: string;
+  options?: Option[];
+}
 
-  export interface DataTableFilterField<TData> {
-    label: string
-    value: keyof TData
-    placeholder?: string
-    options?: Option[]
-  }
+export interface DataTableFilterOption<TData> {
+  id: string;
+  label: string;
+  value: keyof TData;
+  options: Option[];
+  filterValues?: string[];
+  filterOperator?: string;
+  isMulti?: boolean;
+}
