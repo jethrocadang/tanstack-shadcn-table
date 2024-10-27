4613654cdb9e85e253664cc36d2508d51848d7a9
jethrocadang <jethrocadang@gmail.com>
2024-10-27 12:48:05 +0800

    feature(users-table): Added dependencies for the users table

---
+++ src/components/users-table/users-table.tsx
@@ -0,0 +1,11 @@
+import { DataTable } from "../data-table/data-table"
+
+const UsersTable = () => {
+    return (
+        <div>
+            <DataTable/>
+        </div>
+    )
+}
+
+export default UsersTable
